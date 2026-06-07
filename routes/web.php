9a7f229670050568551b98d7ce9f5774281a735b
kuraakenu <krakenxg23@gmail.com>
2026-06-07 15:36:38 +0700

    FrontEnd -Model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,59 +8,12 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
-    $posts = [
-        [
-            'id' => '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Sandhika Galih',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dolorem, nostrum exercitationem asperiores
-            aliquid accusamus id eum amet mollitia repellat quo et consequatur rerum velit non totam, fugiat impedit
-            ipsa?'
-        ],
-        [
-            'id' => '2',
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Sandhika Galih',
-            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, odio possimus corporis perferendis, sequi,
-            inventore rerum architecto accusamus dolores assumenda nisi a! Modi optio suscipit, eos molestias odio at
-            hic!'
-        ]
-    ];
-
-
+    $posts = Post::all();
     return view('posts', ['title' => 'Posts', 'posts' => $posts]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Sandhika Galih',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dolorem, nostrum exercitationem asperiores
-            aliquid accusamus id eum amet mollitia repellat quo et consequatur rerum velit non totam, fugiat impedit
-            ipsa?'
-        ],
-        [
-            'id' => '2',
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Sandhika Galih',
-            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, odio possimus corporis perferendis, sequi,
-            inventore rerum architecto accusamus dolores assumenda nisi a! Modi optio suscipit, eos molestias odio at
-            hic!'
-        ]
-    ];
-
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
-
-    if (!$post) abort(404);
-
+    $post = Post::find($slug);
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
