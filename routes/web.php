0b7f891a1bf94a56fa9ede5a532b2c5977e8310c
kuraakenu <krakenxg23@gmail.com>
2026-06-07 15:19:17 +0700

    FrontEnd -Halaman Single Blog Post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +10,8 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     $posts = [
         [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Sandhika Galih',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dolorem, nostrum exercitationem asperiores
@@ -16,6 +19,8 @@ Route::get('/posts', function () {
             ipsa?'
         ],
         [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Sandhika Galih',
             'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, odio possimus corporis perferendis, sequi,
@@ -27,6 +32,38 @@ Route::get('/posts', function () {
 
     return view('posts', ['title' => 'Posts', 'posts' => $posts]);
 });
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Sandhika Galih',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dolorem, nostrum exercitationem asperiores
+            aliquid accusamus id eum amet mollitia repellat quo et consequatur rerum velit non totam, fugiat impedit
+            ipsa?'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Sandhika Galih',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, odio possimus corporis perferendis, sequi,
+            inventore rerum architecto accusamus dolores assumenda nisi a! Modi optio suscipit, eos molestias odio at
+            hic!'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    if (!$post) abort(404);
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
 
 Route::get('/about', function () {
     return view('about', ['title' => 'About']);
